41dd999bf153001c434e11b4a292d490391bb1f5
Alan Cruz <alansasuke0@gmail.com>
2023-07-13 03:06:25 -0600

    metodos ascender y descender implementados

--- app/Http/Controllers/DivisionController.php
+++ app/Http/Controllers/DivisionController.php
@@ -6,7 +6,8 @@ use App\Models\division;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use App\Models\equipo;
-
+use Illuminate\Support\Facades\DB;
+use PHPUnit\Framework\ActualValueIsNotAnObjectException;
 
 class DivisionController extends Controller
 {
@@ -139,27 +140,93 @@ class DivisionController extends Controller
         $division = $equipo->divisiones()->first();
 
         if ($division) {
-            $nivelActual = $division->pivot->nivel;
+            $nivelActual = $division->nivel;
             if ($nivelActual > 1) {
-                $divisionAnterior = Division::whereHas('equipos', function ($query) use ($nivelActual) {
-                    $query->where('nivel', $nivelActual - 1);
-                })->first();
-                $equipo->divisiones()->sync([$divisionAnterior->id]);
+                $divisionAnterior = Division::where('nivel', $nivelActual - 1)->first();
+                
+                if ($divisionAnterior) {
+                    $equipo->divisiones()->sync([$divisionAnterior->id]);
+
+                    return response()->json([
+                        'msg' => 'Ascendido correctamente',
+                        'data' => $divisionAnterior,
+                        'status' => 200
+                    ], 200);
+                }
+                return response()->json([
+                    'message' => 'El equipo ya se encuentra en el nivel más alto de la división',
+                    'data' => $division,
+                    'status' => 200
+                ], 200);
             } else {
                 return response()->json([
                     'message' => 'El equipo ya se encuentra en el nivel más alto de la división',
+                    'data' => $division,
                     'status' => 200
                 ], 200);
             }
         } else {
-            // El equipo no tiene una división asignada
+
             $divisionMasAlta = Division::orderBy('nivel', 'desc')->first();
             $equipo->divisiones()->sync([$divisionMasAlta->id]);
         }
-
         return response()->json([
-            'message' => 'El equipo ha ascendido correctamente',
+            'msg' => 'ascendido correctamente',
+            'data' => $division,
             'status' => 200
         ], 200);
+    }
+
+    public function descender(int $equipoID)
+    {
+        $equipo = Equipo::find($equipoID);
+
+        if (!$equipo) {
+            return response()->json([
+                'message' => 'Equipo no encontrado',
+                'status' => 404
+            ], 404);
+        }
+
+        $division = $equipo->divisiones()->first();
+
+        if ($division) {
+            $nivelActual = $division->nivel;
+
+            if ($nivelActual >= 1) {
+                $divisionAnterior = Division::where('nivel', $nivelActual + 1)->first();
+
+                if ($divisionAnterior) {
+                    $equipo->divisiones()->sync([$divisionAnterior->id]);
+
+                    return response()->json([
+                        'msg' => 'Descendido correctamente',
+                        'data' => $divisionAnterior,
+                        'status' => 200
+                    ], 200);
+                } else {
+                    // Eliminar registro en tabla pivot
+                    $equipo->divisiones()->detach();
+
+                    return response()->json([
+                        'msg' => 'Equipo eliminado de la división',
+                        'data' => null,
+                        'status' => 200
+                    ], 200);
+                }
+            } else {
+                return response()->json([
+                    'message' => 'El equipo ya se encuentra en el nivel más bajo de la división',
+                    'data' => $division,
+                    'status' => 200
+                ], 200);
+            }
+        } else {
+            return response()->json([
+                'message' => 'El equipo no tiene una división asignada',
+                'data' => null,
+                'status' => 200
+            ], 200);
+        }
     }
 }
